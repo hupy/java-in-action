3b27c916b180062520260d7845527a8e8c6e8044
xinghalo <xinghalo@163.com>
2016-10-19 22:19:12 +0800

    IOUtils

--- src/cn/xingoo/learn/commons/IOUtilsTest.java
+++ src/cn/xingoo/learn/commons/IOUtilsTest.java
@@ -1,6 +1,11 @@
 package cn.xingoo.learn.commons;
 
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by xxh on 2016/10/19.
@@ -8,6 +13,58 @@ import org.junit.Test;
 public class IOUtilsTest {
     @Test
     public void skipTest(){
+        InputStream is = IOUtils.toInputStream("hello world");
+        try {
+            IOUtils.skip(is,4);
+            System.out.println(IOUtils.toString(is,"utf-8"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    @Test
+    public void skipFullyTest(){
+        InputStream is = IOUtils.toInputStream("hello world");
+        try {
+            IOUtils.skipFully(is,30);
+            System.out.println(IOUtils.toString(is,"utf-8"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void writeTest(){
+        try {
+            OutputStream os = new FileOutputStream("E:/test.txt");
+            IOUtils.write("hello write!",os);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void writeLinesTest() throws IOException {
+        List<String> lines = new ArrayList();
+        lines.add("hello");
+        lines.add("list");
+        lines.add("to");
+        lines.add("file");
+        OutputStream os = new FileOutputStream("E:/test.txt");
+        IOUtils.writeLines(lines,IOUtils.LINE_SEPARATOR,os);
+    }
+
+    @Test
+    public void contentEqualsTest(){
+        InputStream is1 = IOUtils.toInputStream("hello123");
+        InputStream is2 = IOUtils.toInputStream("hello123");
+
+        try {
+            System.out.println(IOUtils.contentEquals(is1,is2));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
