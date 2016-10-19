7a1c8450dfe57ae331d4f480456efd16536e3499
xinghalo <xinghalo@163.com>
2016-10-19 22:01:07 +0800

    建个专门学习JDK的项目

---
+++ src/cn/xingoo/learn/commons/IOUtilsTest.java
@@ -0,0 +1,13 @@
+package cn.xingoo.learn.commons;
+
+import org.junit.Test;
+
+/**
+ * Created by xxh on 2016/10/19.
+ */
+public class IOUtilsTest {
+    @Test
+    public void skipTest(){
+
+    }
+}
