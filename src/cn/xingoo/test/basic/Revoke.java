2ed4ccac3db311f81ec91cd998f74610a2f37867
xinghalo <xinghalo@163.com>
2018-02-26 23:03:34 +0800

    nio学习

---
+++ src/cn/xingoo/test/basic/Revoke.java
@@ -0,0 +1,40 @@
+package cn.xingoo.test.basic;
+
+import org.junit.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class Revoke {
+    @Test
+    public void test1() throws NoSuchMethodException, IllegalAccessException, InstantiationException, InvocationTargetException {
+        String s="A";
+        Class clazz =Revoke.class;
+        Method method =clazz.getMethod(s);
+        method.invoke(clazz.newInstance());
+    }
+
+    @Test
+    public void test2() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Stu st1 = new Stu("zhangsan");
+        Class clazz = st1.getClass();
+        Method method = clazz.getMethod("test");
+        method.invoke(st1);
+    }
+
+    public void A(){
+        System.out.println("方法A执行了");
+    }
+
+    class Stu{
+        public String name;
+        public Stu(){}
+        public Stu(String name){
+            this.name = name;
+        }
+        public void test(){
+            System.out.println("my name is "+name);
+        }
+    }
+
+}
